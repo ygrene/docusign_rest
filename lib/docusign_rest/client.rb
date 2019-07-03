ac2b849ff140ffe915fc6ecaf8556f5cb209dbf2
Sreepal Mamunuri <sreepal.mamunuri@sumerusolutions.com>
2019-07-03 18:24:29 +0530

    YT-11509 update notification settings on envelope (#18)
    * YT-11509 update notification settings on envelope
    
    * Review changes

--- lib/docusign_rest/client.rb
+++ lib/docusign_rest/client.rb
@@ -1596,6 +1596,24 @@ module DocusignRest
       JSON.parse(response.body)
     end
 
+    # Public: Update expiration details on envelope
+    def update_notification_settings(options={})
+      content_type = {'Content-Type' => 'application/json'}
+      uri = build_uri("/accounts/#{@acct_id}/envelopes/#{options[:envelope_id]}/notification")
+      post_body = {
+        expirations: {
+          expireEnabled: 'true',
+          expireAfter: options[:expire_after] || 120
+        }
+      }.to_json
+
+      http = initialize_net_http_ssl(uri)
+      request = Net::HTTP::Put.new(uri.request_uri, headers(content_type))
+      request.body = post_body
+
+      response = http.request(request)
+    end
+
     private
 
     def get_id_check_information_input(input)
